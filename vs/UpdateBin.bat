a253beb9e55efcbc6ad28f79d4fb836652e17a7f
David Cofer <dcofer@NeuroRoboticTech.com>
2014-05-19 05:17:06 -0500

    modified update bin

--- vs/UpdateBin.bat
+++ vs/UpdateBin.bat
@@ -1,6 +1,11 @@
 copy Debug\openFrameworksArduinoD.dll ..\..\..\AnimatLabPublicSource\bin\openFrameworksArduinoD.dll
-copy Release\openFrameworksArduino.dll ..\..\..\AnimatLabPublicSource\bin\\openFrameworksArduino.dll
+copy Release\openFrameworksArduino.dll ..\..\..\AnimatLabPublicSource\bin\openFrameworksArduino.dll
+copy Debug\openFrameworksArduinoD.lib ..\..\..\AnimatLabPublicSource\lib\openFrameworksArduinoD.lib
+copy Release\openFrameworksArduino.lib ..\..\..\AnimatLabPublicSource\lib\openFrameworksArduino.lib
+
 copy Debug\openFrameworksArduinoD_x64.dll ..\..\..\AnimatLabPublicSource\bin_x64\openFrameworksArduinoD_x64.dll
 copy Release\openFrameworksArduino_x64.dll ..\..\..\AnimatLabPublicSource\bin_x64\openFrameworksArduino_x64.dll
+copy Debug\openFrameworksArduinoD_x64.lib ..\..\..\AnimatLabPublicSource\lib\openFrameworksArduinoD_x64.lib
+copy Release\openFrameworksArduino_x64.lib ..\..\..\AnimatLabPublicSource\lib\openFrameworksArduino_x64.lib
 
 
