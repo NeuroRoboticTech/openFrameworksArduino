e2464a526534159eff09f6494e0d64427c125a64
David Cofer <dcofer@NeuroRoboticTech.com>
2014-05-26 06:55:00 -0500

    Added a makefile for the project on linux.

--- linux/GenerateSolution.bat
+++ linux/GenerateSolution.bat
@@ -1,5 +1,6 @@
 echo "Generating openFrameworksArduino Solutions"
 
 ..\..\..\AnimatLabPublicSource\bin\premake4 --os=linux --file=openFrameworksArduino.lua codeblocks
+..\..\..\AnimatLabPublicSource\bin\premake4 --os=linux --file=openFrameworksArduino.lua gmake
 
 @pause
